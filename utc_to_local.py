db3f54fb74c9e7c7882992a2dbf90e0be0d7467d
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-01-27 01:27:47 -0800

    string, not array

--- utc_to_local.py
+++ utc_to_local.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import pytz
 
 def dt_utc_str_to_local(dt_str):
-    dt = datetime.strptime(' '.join(dt_str), '%Y-%m-%d_%H.%M.%S')
+    dt = datetime.strptime(dt_str, '%Y-%m-%d_%H.%M.%S')
     dt = dt.replace(tzinfo=pytz.timezone('UTC'))
     return dt.astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %I:%M %p')
 
-print dt_utc_str_to_local(sys.argv[1:])
+print dt_utc_str_to_local(' '.join(sys.argv[1:]))
