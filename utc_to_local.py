3f79d4b3cd1f77e14dfc00af42b23ee1260d2571
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-01-27 01:36:19 -0800

    actually just the hour

--- utc_to_local.py
+++ utc_to_local.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 import pytz
 
-def dt_utc_str_to_local(dt_str):
-    dt = datetime.strptime(dt_str, '%Y-%m-%d_%H.%M.%S')
-    dt = dt.replace(tzinfo=pytz.timezone('UTC'))
-    return dt.astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %I:%M %p')
+def dt_utc_str_to_local(hour):
+    dt = datetime.now()
+    dt = dt.replace(hour=hour, tzinfo=pytz.timezone('UTC'))
+    return dt.astimezone(pytz.timezone('US/Pacific')).strftime('%I %p US/Pacific')
 
-print dt_utc_str_to_local(' '.join(sys.argv[1:]))
+print dt_utc_str_to_local(int(sys.argv[1]))
