5bef0329b45914270ec15948df312bce8cc69a3d
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-01-27 01:25:52 -0800

    first

---
+++ utc_to_local.py
@@ -0,0 +1,11 @@
+import sys
+from datetime import datetime
+
+import pytz
+
+def dt_utc_str_to_local(dt_str):
+    dt = datetime.strptime(' '.join(dt_str), '%Y-%m-%d_%H.%M.%S')
+    dt = dt.replace(tzinfo=pytz.timezone('UTC'))
+    return dt.astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %I:%M %p')
+
+print dt_utc_str_to_local(sys.argv[1:])
